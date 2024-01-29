0adf1cbeb46f26890abe18d200fb6fe75441c925
Amul <amulgairola100@gmail.com>
2024-01-29 15:51:20 +0530

    inital commit

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import './sidebar.css';
-const Sidebar = () => {
-  return (
-    <div className="sideBarContainer">
-        <div className="title">SideBar</div>
-        <div className="SideBarItems">
-            <span></span>
-            <p>Dashboard.</p>
-            <p>Shortcuts.</p>
-            <p>Overview.</p>
-            <p>Profile.</p>
-            <p>Settings.</p>
-        </div>
-    </div>
-  )
-}
+// Sidebar.js
 
-export default Sidebar
+import React, { useState } from 'react';
+import './sidebar.css';
+import menu_icon from './icons/menu.svg'
+const Sidebar = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
+
+    return (
+        <>
+            <div className={`toggleButton ${showSidebar ? 'hide' : ''}`} onClick={toggleSidebar}>
+                <img src={menu_icon} alt="" />
+            </div>
+            <div className={`sideBarContainer ${showSidebar ? 'show' : ''}`}>
+                <div className="title">SideBar</div>
+                <div className="SideBarItems">
+                    <span></span>
+                    <p>Dashboard</p>
+                    <p>Shortcuts</p>
+                    <p>Overview</p>
+                    <p>Profile</p>
+                    <p>Settings</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Sidebar;
